50a5395971a5a51368d31623b09e73e136aaa8f0
Lee <s110919016@stu.ntue.edu.tw>
2022-03-03 10:53:43 +0800

    json

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { StyleSheet, Text, View, Image,ScrollView } from "react-native";
-
+import AlbumData from "../json/albumlist.json";
 const Albumlist=()=>{
     return(
         <ScrollView style={styles.backGroundStyle}>
-            {/* --------------1-------------- */}
             <View style={styles.cardContainerStyle}>
               
                 <View style={styles.cardSectionStyle}>
                     <Image style={styles.imageStyle}
-                    source={{uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bcc96d39-785d-4796-bb89-fede2ff7fe12/deawoz5-4cbea740-a36c-4cb0-be29-b736c51f34a6.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2JjYzk2ZDM5LTc4NWQtNDc5Ni1iYjg5LWZlZGUyZmY3ZmUxMlwvZGVhd296NS00Y2JlYTc0MC1hMzZjLTRjYjAtYmUyOS1iNzM2YzUxZjM0YTYuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.a8gu5YBvWEPkc-wI32Ye2tVIiUeOKl94EloWt4MhSLo",}}
+                    source={{uri: AlbumData[0].image}}
                     />
                 </View> 
                 
@@ -18,14 +17,14 @@ const Albumlist=()=>{
                             <Image
                                 style={styles.thumbnailStyle}
                                 source={{
-                                uri: "http://tqareer.net/wp-content/uploads/2017/07/twice-logo.jpg",
+                                uri: AlbumData[0].logo
                                 }}
                             />
                             
                         </View> 
                         <View>
-                            <Text style={styles.textStyle1}>MORE&MORE</Text>
-                            <Text style={styles.textStyle2}>TWICE</Text>
+                            <Text style={styles.textStyle1}>{AlbumData[0].title}</Text>
+                            <Text style={styles.textStyle2}>{AlbumData[0].artist}</Text>
                         </View>
                         
                 </View>
@@ -38,7 +37,7 @@ const Albumlist=()=>{
                
                 <View style={styles.cardSectionStyle}>
                     <Image style={styles.imageStyle}
-                    source={{uri: "https://kgasa.com/wp-content/uploads/2021/10/Formula-of-Love-OT3.jpg",}}
+                    source={{uri: AlbumData[1].image}}
                     />
                 </View> 
                 <View style={styles.headerContentStyle2}>
@@ -46,14 +45,14 @@ const Albumlist=()=>{
                                     <Image
                                         style={styles.thumbnailStyle}
                                         source={{
-                                        uri: "http://tqareer.net/wp-content/uploads/2017/07/twice-logo.jpg",
+                                            uri: AlbumData[1].logo
                                         }}
                                     />
                                     
                         </View>
                         <View>
-                            <Text style={styles.textStyle3}> Scientist</Text>
-                            <Text style={styles.textStyle4}>TWICE</Text>
+                            <Text style={styles.textStyle3}>{AlbumData[1].title}</Text>
+                            <Text style={styles.textStyle4}>{AlbumData[1].artist}</Text>
                         </View>
                    
                 </View>
@@ -65,7 +64,7 @@ const Albumlist=()=>{
               
                 <View style={styles.cardSectionStyle}>
                     <Image style={styles.imageStyle}
-                    source={{uri: "https://upload.wikimedia.org/wikipedia/zh/7/78/Feel_Special.jpg",}}
+                    source={{uri: AlbumData[2].image}}
                     />
                 </View> 
                 <View style={styles.headerContentStyle3}>
@@ -73,14 +72,14 @@ const Albumlist=()=>{
                             <Image
                                 style={styles.thumbnailStyle}
                                 source={{
-                                uri: "http://tqareer.net/wp-content/uploads/2017/07/twice-logo.jpg",
+                                    uri: AlbumData[2].logo
                                  }}
                             />
                                         
                     </View>
                     <View>
-                        <Text style={styles.textStyle5}>Feel Special</Text>
-                        <Text style={styles.textStyle6}>TWICE</Text>
+                        <Text style={styles.textStyle5}>{AlbumData[2].title}</Text>
+                        <Text style={styles.textStyle6}>{AlbumData[2].artist}</Text>
                     </View>
                  
                 </View>
@@ -94,7 +93,7 @@ const Albumlist=()=>{
             
                 <View style={styles.cardSectionStyle}>
                     <Image style={styles.imageStyle}
-                    source={{uri: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/f1c31a81-4afe-49b1-a6b4-5eb06b074adb/dcrdnqt-9e8d6f97-07c8-4dd7-887d-12d266d1aef9.png/v1/fill/w_800,h_800,q_80,strp/twice___yes_or_yes_by_herestodesign_dcrdnqt-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvZjFjMzFhODEtNGFmZS00OWIxLWE2YjQtNWViMDZiMDc0YWRiXC9kY3JkbnF0LTllOGQ2Zjk3LTA3YzgtNGRkNy04ODdkLTEyZDI2NmQxYWVmOS5wbmciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.KWHyoVNb91Z4WKZK4YgY0_9la-kngcSUsOemWxmR5GM",}}
+                    source={{uri: AlbumData[3].image}}
                     />
                 </View> 
                 <View style={styles.headerContentStyle4}>
@@ -102,15 +101,15 @@ const Albumlist=()=>{
                             <Image
                                 style={styles.thumbnailStyle}
                                 source={{
-                                uri: "http://tqareer.net/wp-content/uploads/2017/07/twice-logo.jpg",
+                                    uri: AlbumData[3].logo
                                  }}
                             />
                                         
                         </View>
 
                     <View>
-                        <Text style={styles.textStyle7}>YES or YES </Text>
-                        <Text style={styles.textStyle8}>TWICE</Text>
+                        <Text style={styles.textStyle7}>{AlbumData[3].title}</Text>
+                        <Text style={styles.textStyle8}>{AlbumData[3].artist}</Text>
                     </View>
                    
                 </View>
@@ -122,24 +121,24 @@ const Albumlist=()=>{
        
                 <View style={styles.cardSectionStyle}>
                     <Image style={styles.imageStyle}
-                    source={{uri: "https://pm1.narvii.com/6665/a00459541a365fef5db56446c3761d78e5c3ec22_hq.jpg",}}
+                    source={{uri: AlbumData[4].image}}
                     />
                 </View> 
                 <View style={styles.headerContentStyle5}> 
                     <View style={styles.thumbnailContainerStyle}>
-                            <Image
-                                style={styles.thumbnailStyle}
-                                source={{
-                                uri: "http://tqareer.net/wp-content/uploads/2017/07/twice-logo.jpg",
-                                 }}
-                            />
+                        <Image
+                            style={styles.thumbnailStyle}
+                            source={{
+                                uri: AlbumData[4].logo
+                             }}
+                        />
                                         
-                        </View>
+                    </View>
 
 
                     <View>
-                        <Text style={styles.textStyle9}>Heart Shaker</Text>
-                        <Text style={styles.textStyle0}> TWICE</Text>
+                        <Text style={styles.textStyle9}>{AlbumData[4].title}</Text>
+                        <Text style={styles.textStyle0}>{AlbumData[4].artist}</Text>
                     </View>
                
                    
@@ -157,18 +156,7 @@ const styles = StyleSheet.create({
        
     },
 
-    // textStyle: {
-    // //   marginTop:20,
-    //   fontSize: 18, 
-    //   marginLeft:15,
-    //   color:"#f27a7d",
-    // },
-    // text2Style: {
-    //     fontSize: 14,
-    //     marginLeft:16,
-    //     marginTop:5,
-    //   },
-
+    
     // -------------------1----------------
         textStyle1: {
         fontSize: 18, 
@@ -182,7 +170,7 @@ const styles = StyleSheet.create({
             color:"#e27396",
           },
     
-          headerContentStyle1:{
+        headerContentStyle1:{
             
                alignItems: "center", 
                backgroundColor:"#faedcb",
